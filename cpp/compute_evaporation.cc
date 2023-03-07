dac13a03b3c202b857e1bcd9e25b06e0fc1567f4
lydiaslanidou <lydia.aslanidou@gmail.com>
2023-03-07 15:35:34 +0100

    Added evaporation calculations and diameter update to compute_evaporation.cc

--- cpp/compute_evaporation.cc
+++ cpp/compute_evaporation.cc
@@ -1,22 +1,19 @@
 #include "compute_evaporation.hh"
 #include "droplet.hh"
 #include <cmath>
+#include <constants.hh>
 
 ComputeEvaporation::ComputeEvaporation(Real temperature, Real relative_h) : temperature(temperature), relative_h(relative_h) {
-    // TODO: Compute the wet-bulb temperature
-    // d0 = 0.151977
-    // d1 = 8.313659
-    // d2 = 1.676331
-    // d3 = 0.00391838
-    // d4 = 0.023101
-    // d5 = 4.686035
-    wet_bulb_temp = temperature * atan(0.151977 * 0.0);
-    vapor_diff = 0.0;
-    vapor_press = 0.0;
-    film_temp = 0.0;
 
-    alpha = 0.0;
-    beta = 0.0;
+    wet_bulb_temp = temperature*atan(0.151977*pow((relative_h+8.313659),0.5)) + atan(temperature + relative_h) - atan(relative_h - 1.676331) + 0.00391838*pow(relative_h,1.5)*atan(0.023101*relative_h) - 4.686035;
+
+    vapor_diff = 21.2e-6 * (1 + 0.0071 * wet_bulb_temp);
+    vapor_press = 67 * (temperature - wet_bulb_temp);
+    film_temp = wetBulb + 1/3*(temperature-wet_bulb_temp);
+
+    alpha = 4 * WATER_MOL_W * vapor_diff * vapor_press / (DENSITY_L * R * (film_temp + 273.15));
+    beta = 0.276 * pow ((DENSITY_G / (VISC_G * pow(vapor_diff,2))),(1 / 6));
+
 }
 
 void ComputeEvaporation::setParams(Real temperature, Real relative_h) {
@@ -30,12 +27,18 @@ void ComputeEvaporation::compute(System &system) {
         auto &par = dynamic_cast<Droplet &>(system.getParticle(np));
         auto &pos = par.getPosition();
         auto &vel = par.getVelocity();
+        auto &diam = par.getDiameter();
         auto &is_active = par.getState();
 
         if (!is_active)
             break;
 
-        // TODO: implement the `evap` function
+        Real sqrt_arg = pow(diam,2) - 2 * dt * alpha_evap * (1 + beta_evap * sqrt(diam * vel));
+        Real temp_diam = sqrt_arg < 0 ? D_MIN : sqrt(sqrt_arg);
+
+        //TODO: add calculation of rate of change of mass, Mp, needed for blowing velocity
+
+        diam = temp_diam;
     }
 }
 
