91939f3ad4e8b8662da1750df04e6a38098ce358
edoardob90 <edoardob90@gmail.com>
2023-03-07 15:09:30 +0100

    Add physical constants (header-only)

---
+++ cpp/constants.hh
@@ -0,0 +1,12 @@
+#ifndef PARTICLES_CONSTANTS_HH
+#define PARTICLES_CONSTANTS_HH
+
+// Physical constants used throughout the code
+
+static constexpr double DENSITY_L = 1000.0;    // [kg/m3], density of liquid;
+static constexpr double DENSITY_G = 1.0;       // [kg/m3], density of air
+static constexpr double VISC_G = 1.8e-5;       // [kg/m/s], air viscosity
+static constexpr double WATER_MOL_W = 0.01802; // [kg/mol], molecular weight of water
+static constexpr double R = 8.3145;            // [J/mol/K]  or [m3*Pa/K/mol], universal gas constant
+
+#endif
